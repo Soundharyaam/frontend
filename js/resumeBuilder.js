65f1293475e389fb87597afd69d451fb675138be
Soundharya AM <soundharyabharatiya@gmail.com>
2018-04-20 09:59:33 +0530

    console logging characters

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,3 +3,7 @@ This is empty on purpose! Your code to build the resume will go here.
  */
 
 $("#main").append("Soundharya");
+var firstName = "Soundharya";
+var age = 24;
+
+console.log(firstName);
