1383f502d529391dc6ddab3e0110ef6dbed36319
Soundharya AM <soundharyabharatiya@gmail.com>
2018-04-20 11:19:42 +0530

    role and name inclusion

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,7 +2,7 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
-$("#main").append("Soundharya");
+//$("#main").append("Soundharya");
 var firstName = "Soundharya";
 var age = 24;
 
@@ -18,4 +18,16 @@ var awesomeThoughts = "I am " + firstName + " and I am AWESOME!";
 console.log(awesomeThoughts);
 funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
 console.log(funThoughts);
-$("#main").append(funThoughts);
+//$("#main").append(funThoughts);
+
+var name = "Soundharya AM";
+var role = "Software Developer";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+//console.log(formattedName);
+//console.log(formattedRole);
+
+$("#main").append(formattedName);
+$("#main").append(formattedRole);
