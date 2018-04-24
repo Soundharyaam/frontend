811c2bab7e8a3fe469eeef878ffdae466a084759
Soundharya AM <soundharyabharatiya@gmail.com>
2018-04-24 12:52:30 +0530

    adding work with for-in loops

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -206,8 +206,20 @@ while (cameron.job === "course dev") {
 
 console.log(cameron.job);*/
 
+if (work.jobs.length > 0) {
 
+    $("#workExperience").append(HTMLworkStart);
 
-/*for (var i = 0; i < 3; i++) {
-
-}*/
+    for (job in work.jobs) {
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+        var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+        var formattedEmployerTitle = formattedEmployer + formattedTitle;
+        $(".work-entry:last").append(formattedEmployerTitle);
+        var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+        $(".work-entry:last").append(formattedDates);
+        var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+        $(".work-entry:last").append(formattedLocation);
+        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+        $(".work-entry:last").append(formattedDescription);
+    }
+}
