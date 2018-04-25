eb3ea92d270e15035396db5bccf1466ebaec2da9
Soundharya AM <soundharyabharatiya@gmail.com>
2018-04-25 13:00:18 +0530

    internationalize Button is added

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -20,47 +20,8 @@ funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
 console.log(funThoughts);
 //$("#main").append(funThoughts);*/
 
-var name = "Soundharya AM";
-var role = "Software Developer";
-
-var formattedName = HTMLheaderName.replace("%data%", name);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-
 //console.log(formattedName);
 //console.log(formattedRole);
-
-//$("#main").append(formattedName);
-//$("#main").append(formattedRole);
-
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-
-var bio = {
-    "name": "Soundharya",
-    "role": "Software developer",
-    "contactInfo": {
-        "email": "contact@soundharyaam.com",
-        "github": "soundharyaam",
-        "twitter": "soundharyaam",
-        "instagram": "soundharyaam"
-    },
-    "pictureURL": "http://brightdrops.com/wp-content/uploads/2016/12/buddha-the-mind-is-1.jpg",
-    "welcomeMessage": "Hi there! This is Soundharya. Welcome to my website.",
-    "skills": ["software development", "researching", "photography", "writing"]
-}
-
-$("#main").append(bio.contactInfo);
-$("#main").append(bio.pictureURL);
-$("#main").append(bio.welcomeMessage);
-$("#main").append(bio.skills);
-
-var work = {
-    "position": "Senior front-end engineer",
-    "employer": "Astutix",
-    "yearsWorked": "3 years"
-}
-
-work.city = "Bangalore";
 
 /*var education = {
     "schoolName": "Anna University",
@@ -71,39 +32,6 @@ education["schoolCity"] = "Coimbatore";
 
 $("#main").append(work["position"]);
 $("#main").append(education.schoolName);*/
-
-var education = {
-    "school": [{
-            "name": "Anna University",
-            "city": "Coimbatore",
-            "degree": "BE",
-            "major": ["Electronics", "Telecommunications"],
-            "minor": ["Computer Programming", "Robotics"],
-            "years": "2011-2015"
-        },
-        {
-            "name": "Harvard",
-            "city": "Boston, MA",
-            "degree": "MBA",
-            "major": ["International Business, Entrepreneurship"],
-            "minor": ["Finance", "Trading"],
-            "years": "2020-2022"
-        }
-    ],
-    "onlineCourse": [{
-            "title": "Python for Everybody",
-            "school": "University of Michigan",
-            "provider": "Coursera"
-        },
-        {
-            "title": "Introduction to entrepreneurship and business management skills",
-            "school": "Harvard",
-            "provider": "Harvard Business Review"
-        }
-    ]
-}
-
-$("#main").append(education.onlineCourse[0].title);
 
 /*var work = {
     "officeWorks": [{
@@ -138,6 +66,106 @@ $("#main").append(education.onlineCourse[0].title);
     ]
 };*/
 
+/*$("#main").append(projects.variousProjects[1].name);
+$("#main").append(work.officeWorks[1].task);
+$("#main").append(work.openSourceProjects[0].org);*/
+
+/*var cameron = {};
+cameron.job = "course dev";
+
+var makeCourse = function() {
+    console.log("Made a course");
+}
+
+var courses = 0;
+while (cameron.job === "course dev") {
+    makeCourse();
+    courses = courses + 1;
+    if (courses === 10) {
+        cameron.job = "learning specialist";
+    }
+}
+
+console.log(cameron.job);*/
+
+//collecting click locations
+/*
+$(document).click(function(loc) {
+    var x = loc.pageX;
+    var y = loc.pageY;
+
+    logClicks(x, y);
+});*/
+
+var name = "Soundharya Am";
+var role = "Software Developer";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
+
+var bio = {
+    "name": "Soundharya Am",
+    "role": "Software developer",
+    "contactInfo": {
+        "email": "contact@soundharyaam.com",
+        "github": "soundharyaam",
+        "twitter": "soundharyaam",
+        "instagram": "soundharyaam"
+    },
+    "pictureURL": "http://brightdrops.com/wp-content/uploads/2016/12/buddha-the-mind-is-1.jpg",
+    "welcomeMessage": "Hi there! This is Soundharya. Welcome to my website.",
+    "skills": ["software development", "researching", "photography", "writing"]
+}
+
+$("#main").append(bio.contactInfo);
+$("#main").append(bio.pictureURL);
+$("#main").append(bio.welcomeMessage);
+$("#main").append(bio.skills);
+
+var work = {
+    "position": "Senior front-end engineer",
+    "employer": "Astutix",
+    "yearsWorked": "3 years"
+}
+
+work.city = "Bangalore";
+
+var education = {
+    "school": [{
+            "name": "Anna University",
+            "city": "Coimbatore",
+            "degree": "BE",
+            "major": ["Electronics", "Telecommunications"],
+            "minor": ["Computer Programming", "Robotics"],
+            "years": "2011-2015"
+        },
+        {
+            "name": "Harvard",
+            "city": "Boston, MA",
+            "degree": "MBA",
+            "major": ["International Business, Entrepreneurship"],
+            "minor": ["Finance", "Trading"],
+            "years": "2020-2022"
+        }
+    ],
+    "onlineCourse": [{
+            "title": "Python for Everybody",
+            "school": "University of Michigan",
+            "provider": "Coursera"
+        },
+        {
+            "title": "Introduction to entrepreneurship and business management skills",
+            "school": "Harvard",
+            "provider": "Harvard Business Review"
+        }
+    ]
+}
+
+$("#main").append(education.onlineCourse[0].title);
+
 var work = {
     "jobs": [{
             "employer": "abc",
@@ -161,21 +189,17 @@ var projects = {
     "projects": [{
             "title": "studentinno",
             "dates": "Oct 2015- sept 2016",
-            "description": "Responsible for AI and ML processes",
+            "description": "AI and ML process projects",
             "images": "https://martechtoday.com/wp-content/uploads/2018/02/robot-calculations-analytics-ai-ss-1920-800x450.jpg"
         },
         {
             "title": "Soundharyaam.com",
             "dates": "Jan 2017- present",
-            "description": "Responsible for DevOps",
+            "description": "DevOps Projects",
             "images": "https://martechtoday.com/wp-content/uploads/2018/02/robot-calculations-analytics-ai-ss-1920-800x450.jpg"
         }
     ]
 }
-
-/*$("#main").append(projects.variousProjects[1].name);
-$("#main").append(work.officeWorks[1].task);
-$("#main").append(work.openSourceProjects[0].org);*/
 
 $("#main").append(work.jobs[0].title);
 $("#main").append(projects.projects[1].title);
@@ -188,41 +212,26 @@ if (bio.skills.length > 0) {
     }
 }
 
-/*var cameron = {};
-cameron.job = "course dev";
-
-var makeCourse = function() {
-    console.log("Made a course");
-}
-
-var courses = 0;
-while (cameron.job === "course dev") {
-    makeCourse();
-    courses = courses + 1;
-    if (courses === 10) {
-        cameron.job = "learning specialist";
-    }
-}
-
-console.log(cameron.job);*/
-
 function displayWork() {
 
     if (work.jobs.length > 0) {
 
         $("#workExperience").append(HTMLworkStart);
 
-        for (var i = 0; i < 4; i++) {
+        for (var i = 0; i < 2; i++) {
             var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[i].employer);
             var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[i].title);
             var formattedEmployerTitle = formattedEmployer + formattedTitle;
-            $(".work-entry:last").append(formattedEmployerTitle);
+
             var formattedDates = HTMLworkDates.replace("%data%", work.jobs[i].dates);
-            $(".work-entry:last").append(formattedDates);
             var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[i].location);
-            $(".work-entry:last").append(formattedLocation);
             var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[i].description);
-            $(".work-entry:last").append(formattedDescription);
+
+            $(".work-entry:last")
+                .append(formattedEmployerTitle)
+                .append(formattedDates)
+                .append(formattedLocation)
+                .append(formattedDescription);
         }
     }
 
@@ -230,11 +239,14 @@ function displayWork() {
 
 displayWork();
 
-//collecting click locations
+var oldName = name;
 
-$(document).click(function(loc) {
-    var x = loc.pageX;
-    var y = loc.pageY;
+function inName(oldName) {
+    var finalName = oldName;
+    var splitName = finalName.split(" ");
+    finalName = (splitName[0].charAt(0).toUpperCase()) + (splitName[0].slice(1, splitName[0].length).toLowerCase()) + " " + (splitName[1].toUpperCase());
+    return finalName;
+    //console.log(finalName);
+}
 
-    logClicks(x, y);
-});
+$("#main").append(internationalizeButton);
